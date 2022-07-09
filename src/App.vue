61924beb9c1ed169110c647df572fd7b5d1392aa
bianling <1343881168@qq.com>
2022-07-09 17:58:38 +0800

    完成切换页面附加练习

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-link to="/index">首页</router-link>
+    <router-link to="/classification">分类</router-link>
+    <router-link to="/order">订单</router-link>
+    <router-link to="/my">我的</router-link>
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+a {
+  margin-right: 8px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+import My from './views/My.vue'
+import Classification from './views/Classification.vue'
+import Index from './views/Index.vue'
+import Order from './views/Order.vue'
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/my',
+      component: My
+    },
+    {
+      path: '/classification',
+      component: Classification
+    },
+    {
+      path: '/index',
+      component: Index
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+  ]
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
