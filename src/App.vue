8a2d2943126a1a5da7fc28493f48f2adc33244ea
bianling <1343881168@qq.com>
2022-07-09 20:05:09 +0800

    完成手写原理

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
   <div>
-    <router-link to="/ui_router">UI_Router</router-link>
-    <router-link to="/contacts">Contacts</router-link>
-    <router-link to="/about">About</router-link>
-    <hr />
-    <div>
-      <router-view></router-view>
-    </div>
+    <a href="#/myabout">首页</a>
+    <a href="#/myhome">电影</a>
+    <a href="#/mymovie">关于</a>
+    <component :is="comName"> </component>
   </div>
 </template>
 
 <script>
+import MyAbout from "./views/extend/MyAbout.vue";
+import MyHome from "./views/extend/MyHome.vue";
+import MyMovie from "./views/extend/MyMovie.vue";
+
 export default {
   name: "App",
+  data() {
+    return {
+      comName: "MyAbout",
+    };
+  },
+  components: {
+    MyAbout,
+    MyHome,
+    MyMovie,
+  },
+  created() {
+    window.onhashchange = () => {
+      switch (location.hash) {
+        case "#/myabout":
+          this.comName = "MyAbout";
+          break;
+        case "#/myhome":
+          this.comName = "MyHome";
+          break;
+        case "#/mymovie":
+          this.comName = "MyMovie";
+          break;
+      }
+    };
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,16 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
-import UI_Router from './views/3router/UI_Router.vue'
-import Contacts from './views/3router/Contacts.vue'
-import About from './views/3router/About.vue'
-//二级路由
-import Allcontacts from './views/3router/3router-2/Allcontacts.vue'
-import Alice from './views/3router/3router-2/Alice.vue'
-import Bob from './views/3router/3router-2/Bob.vue'
-//三级路由
-import Blog from './views/3router/3router-2/3router-3/Blog.vue'
-import Fax from './views/3router/3router-2/3router-3/Fax.vue'
+
 
 let routerPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
@@ -22,50 +13,7 @@ Vue.config.productionTip = false
 
 const router = new VueRouter({
   routes: [
-    {
-      path: '/',
-      redirect: '/ui_router'
-    },
-    {
-      path: '/ui_router',
-      component: UI_Router
-    },
-    {
-      path: '/contacts',
-      component: Contacts,
-      children: [
-        {
-          path: 'allcontacts',
-          component: Allcontacts,
-          name: 'Allcontacts'
-        },
-        {
-          path: 'alice',
-          component: Alice,
-          name: 'Alice'
 
-        },
-        {
-          path: 'bob',
-          component: Bob,
-          name: 'Bob',
-          children: [
-            {
-              path: 'blog',
-              component: Blog
-            },
-            {
-              path: 'fax',
-              component: Fax
-            },
-          ]
-        },
-      ]
-    },
-    {
-      path: '/about',
-      component: About
-    },
   ]
 })
 
